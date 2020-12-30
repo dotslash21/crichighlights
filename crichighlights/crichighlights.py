d1934592878f6e10c7b7ebe580f90be0f3094152
dotslash21 <arunangshubsws@gmail.com>
2020-12-30 17:45:31 +0530

    Added functionality for getting live match highlights.

--- crichighlights/crichighlights.py
+++ crichighlights/crichighlights.py
@@ -1,6 +1,8 @@
-from typing import List
-
+import re
 import requests
+import time
+from math import ceil
+from typing import List, Generator
 
 
 class CricHighlights:
@@ -15,6 +17,64 @@ class CricHighlights:
             return response
         except Exception:
             raise
+
+    @staticmethod
+    def __match_did_end(match_id: str) -> bool:
+        """Method to check if the match has ended.
+
+        Args:
+            match_id (str): The ID of the match.
+
+        Returns:
+            bool: True if the match has ended, False otherwise.
+
+       """
+
+        url = 'http://mapps.cricbuzz.com/cbzios/match/' + match_id
+
+        # fetch data from the endpoint.
+        data = CricHighlights.__crawl(url)
+        status = data.get('header').get('status')
+
+        # Check the match status to determine match end
+        regex = re.compile(r'\bwon\b | \blost\b | \bdraw\b', flags=re.I | re.X)
+        matches = regex.findall(status)
+        if matches:
+            return True
+
+        return False
+
+    @staticmethod
+    def __splice_highlight(highlights: dict, last_timestamp: int) -> List[dict]:
+        """Method to splice out the latest highlights from a list of highlights.
+
+        Args:
+            highlights (dict): The highlights of the match.
+            last_timestamp (int): The timestamp of the last received highlight.
+
+        Yields:
+            dict: The list of new highlight data.
+
+       """
+
+        low = 0
+        high = len(highlights) - 1
+
+        # Check if there has been new update.
+        if int(highlights[low].get('timestamp')) == last_timestamp:
+            return []
+
+        # Perform binary search to find the number of new highlight updates.
+        while low < high:
+            mid = low + ceil((high - low) / 2.0)
+            curr_timestamp = int(highlights[mid].get('timestamp'))
+
+            if curr_timestamp > last_timestamp:
+                low = mid
+            else:
+                high = mid - 1
+
+        return highlights[:low + 1]
 
     @staticmethod
     def get_live_matches() -> List[dict]:
@@ -52,3 +112,48 @@ class CricHighlights:
                 }
             })
         return result
+
+    @staticmethod
+    def get_highlights(match_id: str) -> Generator[dict, None, None]:
+        """Method to get match highlights.
+
+        Args:
+            match_id (str): The ID of the match.
+
+        Yields:
+            dict: The highlight data.
+
+       """
+
+        url = 'http://mapps.cricbuzz.com/cbzios/match/' + match_id + '/commentary'
+
+        # Check if the match already ended.
+        if CricHighlights.__match_did_end(match_id):
+            # fetch data from the endpoint.
+            data = CricHighlights.__crawl(url)
+            highlights = data.get('comm_lines')
+
+            # yield the highlights
+            for highlight in reversed(highlights):
+                yield highlight
+
+            # Raise StopIteration
+            return
+
+        # Keep fetching highlights until match end.
+        last_timestamp = 0  # Track the timestamp of the last received highlight.
+        while not CricHighlights.__match_did_end(match_id):
+            # fetch data from the endpoint.
+            data = CricHighlights.__crawl(url)
+            highlights = data.get('comm_lines')
+
+            # Yield any new received highlight.
+            new_highlights = CricHighlights.__splice_highlight(highlights, last_timestamp)
+            for highlight in reversed(new_highlights):
+                yield highlight
+
+            # Update the last_timestamp.
+            last_timestamp = int(highlights[0].get('timestamp'))
+
+            # Wait for 10 seconds before fetching data again.
+            time.sleep(10)
