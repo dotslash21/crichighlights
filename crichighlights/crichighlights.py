0eec6eed3fc2052ea3d28878795e6be68775220b
dotslash21 <arunangshubsws@gmail.com>
2020-12-30 19:55:17 +0530

    Added exception handling in library.

--- crichighlights/crichighlights.py
+++ crichighlights/crichighlights.py
@@ -1,8 +1,9 @@
 import re
-import requests
 import time
 from math import ceil
 from typing import List, Generator
+
+import requests
 
 
 class CricHighlights:
@@ -20,13 +21,16 @@ class CricHighlights:
         Returns:
             dict: The fetched JSON data as Python dict.
 
+        Raises:
+            Exception: Error occurred while fetching the data.
+
        """
 
         try:
             response = requests.get(url).json()
             return response
-        except Exception:
-            raise
+        except Exception as e:
+            raise e
 
     @staticmethod
     def __splice_highlight(highlights: dict, last_timestamp: int) -> List[dict]:
@@ -36,7 +40,7 @@ class CricHighlights:
             highlights (dict): The highlights of the match.
             last_timestamp (int): The timestamp of the last received highlight.
 
-        Yields:
+        Returns:
             dict: The list of new highlight data.
 
        """
@@ -70,18 +74,24 @@ class CricHighlights:
         Returns:
             bool: True if the match id is valid, False otherwise.
 
+        Raises:
+            Exception: Error occurred while fetching the data.
+
        """
 
         url = 'http://mapps.cricbuzz.com/cbzios/match/' + match_id
 
-        # fetch data from the endpoint.
-        data = CricHighlights.__crawl(url)
+        try:
+            # fetch data from the endpoint.
+            data = CricHighlights.__crawl(url)
 
-        # Perform check
-        if match_id != data.get('match_id'):
-            return False
+            # Perform check
+            if match_id != data.get('match_id'):
+                return False
 
-        return True
+            return True
+        except Exception as e:
+            raise e
 
     @staticmethod
     def match_did_end(match_id: str) -> bool:
@@ -93,21 +103,27 @@ class CricHighlights:
         Returns:
             bool: True if the match has ended, False otherwise.
 
+        Raises:
+            Exception: Error occurred while fetching the data.
+
        """
 
         url = 'http://mapps.cricbuzz.com/cbzios/match/' + match_id
 
-        # fetch data from the endpoint.
-        data = CricHighlights.__crawl(url)
-        status = data.get('header').get('status')
+        try:
+            # fetch data from the endpoint.
+            data = CricHighlights.__crawl(url)
+            status = data.get('header').get('status')
 
-        # Check the match status to determine match end
-        regex = re.compile(r'\bwon\b | \blost\b | \bdraw\b', flags=re.I | re.X)
-        matches = regex.findall(status)
-        if matches:
-            return True
+            # Check the match status to determine match end
+            regex = re.compile(r'\bwon\b | \blost\b | \bdraw\b', flags=re.I | re.X)
+            matches = regex.findall(status)
+            if matches:
+                return True
 
-        return False
+            return False
+        except Exception as e:
+            raise e
 
     @staticmethod
     def match_did_start(match_id: str) -> bool:
@@ -119,18 +135,24 @@ class CricHighlights:
         Returns:
             bool: True if the match has started, False otherwise.
 
+        Raises:
+            Exception: Error occurred while fetching the data.
+
        """
 
         url = 'http://mapps.cricbuzz.com/cbzios/match/' + match_id
 
-        # fetch data from the endpoint.
-        data = CricHighlights.__crawl(url)
-        state = data.get('header').get('state')
+        try:
+            # fetch data from the endpoint.
+            data = CricHighlights.__crawl(url)
+            state = data.get('header').get('state')
 
-        if state == 'preview':
-            return False
+            if state == 'preview':
+                return False
 
-        return True
+            return True
+        except Exception as e:
+            raise e
 
     @staticmethod
     def get_live_matches() -> List[dict]:
@@ -139,35 +161,41 @@ class CricHighlights:
         Returns:
             List[dict]: List of live match data.
 
+        Raises:
+            Exception: Error occurred while fetching the data.
+
         """
 
         # Endpoint for getting live matches.
         url = "http://mapps.cricbuzz.com/cbzios/match/livematches"
 
-        # fetch data from the endpoint.
-        data = CricHighlights.__crawl(url)
+        try:
+            # fetch data from the endpoint.
+            data = CricHighlights.__crawl(url)
 
-        # Extract the match data and return the relevant information
-        matches = data['matches']
-        result = []
-        for match in matches:
-            result.append({
-                'match_id': match.get('match_id'),
-                'series_id': match.get('series_id'),
-                'series_name': match.get('series_name'),
-                'match_desc': match.get('header').get('match_desc'),
-                'status': match.get('header').get('status'),
-                'type': match.get('header').get('type'),
-                'team1': {
-                    'name': match.get('team1').get('name'),
-                    's_name': match.get('team1').get('s_name')
-                },
-                'team2': {
-                    'name': match.get('team2').get('name'),
-                    's_name': match.get('team2').get('s_name')
-                }
-            })
-        return result
+            # Extract the match data and return the relevant information
+            matches = data['matches']
+            result = []
+            for match in matches:
+                result.append({
+                    'match_id': match.get('match_id'),
+                    'series_id': match.get('series_id'),
+                    'series_name': match.get('series_name'),
+                    'match_desc': match.get('header').get('match_desc'),
+                    'status': match.get('header').get('status'),
+                    'type': match.get('header').get('type'),
+                    'team1': {
+                        'name': match.get('team1').get('name'),
+                        's_name': match.get('team1').get('s_name')
+                    },
+                    'team2': {
+                        'name': match.get('team2').get('name'),
+                        's_name': match.get('team2').get('s_name')
+                    }
+                })
+            return result
+        except Exception as e:
+            raise e
 
     @staticmethod
     def get_highlights(match_id: str) -> Generator[dict, None, None]:
@@ -179,28 +207,34 @@ class CricHighlights:
         Yields:
             dict: The highlight data.
 
+        Raises:
+            Exception: Error occurred while fetching the data.
+
        """
 
         url = 'http://mapps.cricbuzz.com/cbzios/match/' + match_id + '/commentary'
 
-        # Stop if the provided match id is not valid
-        if not CricHighlights.is_match_id_valid(match_id):
-            return
+        try:
+            # Stop if the provided match id is not valid
+            if not CricHighlights.is_match_id_valid(match_id):
+                return
 
-        # Keep fetching highlights until match end.
-        last_timestamp = 0  # Track the timestamp of the last received highlight.
-        while not CricHighlights.match_did_end(match_id) or last_timestamp == 0:
-            # fetch data from the endpoint.
-            data = CricHighlights.__crawl(url)
-            highlights = data.get('comm_lines')
+            # Keep fetching highlights until match end.
+            last_timestamp = 0  # Track the timestamp of the last received highlight.
+            while not CricHighlights.match_did_end(match_id) or last_timestamp == 0:
+                # fetch data from the endpoint.
+                data = CricHighlights.__crawl(url)
+                highlights = data.get('comm_lines')
 
-            # Yield any new received highlight.
-            new_highlights = CricHighlights.__splice_highlight(highlights, last_timestamp)
-            for highlight in reversed(new_highlights):
-                yield highlight
+                # Yield any new received highlight.
+                new_highlights = CricHighlights.__splice_highlight(highlights, last_timestamp)
+                for highlight in reversed(new_highlights):
+                    yield highlight
 
-            # Update the last_timestamp.
-            last_timestamp = int(highlights[0].get('timestamp'))
+                # Update the last_timestamp.
+                last_timestamp = int(highlights[0].get('timestamp'))
 
-            # Wait for 10 seconds before fetching data again.
-            time.sleep(10)
+                # Wait for 10 seconds before fetching data again.
+                time.sleep(10)
+        except Exception as e:
+            raise e
